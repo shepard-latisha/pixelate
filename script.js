94e56785e58bec4bddd234367bb164abd41b6841
nazhang8888 <nazhang@pm.me>
2023-02-19 16:55:47 -0500

    added button functionality

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // Your code here
+const table = document.getElementsByTagName("table")[0];
+
+function makeRow() {
+  const row = document.createElement("tr");
+
+  for (let i = 0; i < 20; i++) {
+    const td = document.createElement("td");
+    row.appendChild(td);
+  }
+
+  table.appendChild(row);
+}
+
+const addRowButton = document.getElementById("add-row");
+addRowButton.addEventListener("click", makeRow);
